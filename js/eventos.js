a02e07512953beca7d70682f2b1958b8b73dd29b
LuciaValverdeCP <lvalverde.lez@gmail.com>
2024-08-10 00:53:23 -0300

    solucion-click

--- js/eventos.js
+++ js/eventos.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    document.querySelector("div").addEventListener("click", function(){
-        alert("Hola! Soy el div")
-    })
+    document.querySelector("div").addEventListener('click', function(){
+        alert("Hola! Soy el div");
+    });
 
+    document.querySelector('button').addEventListener('click', function(eventoClick) {
+        eventoClick.stopPropagation();
+    });
 })
